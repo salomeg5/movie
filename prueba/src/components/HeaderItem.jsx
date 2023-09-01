6cd97fc05ec5168463a49890e6ac1008fd6ab719
sgallegoa@unitecnica.net <sgallegoa@unitecnica.net>
2023-09-01 10:58:20 -0500

    Efecto Menú
    Efecto de click

--- prueba/src/components/HeaderItem.jsx
+++ prueba/src/components/HeaderItem.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 function HeaderItem({name,Icon}) {
   return (
-    <div className='text-white flex items-center gap-3
-    text-[15px] font-semibold cursor-pointer hover:underline
-    underline-offset-8 mb-2'>
+    <div className='text-white  flex items-center gap-3 text-[15px] font-semibold cursor-pointer transition-all hover:scale-50 hover:underline  underline-offset-8 mb-2 '>
         <Icon/>
         <h2 className=''>{name}</h2>
     </div>
